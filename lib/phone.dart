d2d6d402be4176411494c2b9195dd3fc5dd8cbbe
Mohsen Ghalem <ghalemmohsen@gmail.com>
2022-06-15 12:54:41 +0100

    Search Delete PrintSpec Updated
    Fixed All Below Fontction :
    
    Delete Fun
    Search Fun (by name, by phone)
    PrintSpecifc Contact

--- lib/phone.dart
+++ lib/phone.dart
@@ -1,7 +1,5 @@
 import 'dart:io';
 
-import 'package:quiz/quiz.dart';
-
 class Contacts {
   int phoneNumber;
   String name;
@@ -26,14 +24,15 @@ class Operation {
 
   void addContact() {
     _contacts.add(Contacts(phoneNumber, birthday, name));
-
-    print("Contact Adedd !");
+    print("Adding Contact....\n");
+    sleep(Duration(seconds: 2));
+    print("Contact Added !\n");
   }
 
   void printAllContact() {
-    if (_contacts == null)
+    if (_contacts.isEmpty) {
       print('There is not contacts');
-    else {
+    } else {
       for (int i = 0; i < _contacts.length; i++) {
         print("---- Contact ${i + 1} ----\n");
         print(
@@ -43,26 +42,58 @@ class Operation {
   }
 
   void deleteContact() {
-
-    _contacts.removeAt(index);
-
-
+    int position = searchContact();
+    if (_contacts.isEmpty) {
+      print("There's no contact to delete !");
+    }
+    else {
+      if (position >= 0) {
+        _contacts.removeAt(position);
+        print("Removing Contact....\n");
+        sleep(Duration(seconds: 2));
+        print("Contact Removed !\n");
+      } else {
+        print("Error! : The Contact Doesn't Exist\n");
+      }
+    }
   }
 
-  void searchContact() {
-    _contacts.contains()
+  int searchContact() {
+    int i;
+    for (i = 0; i < _contacts.length; i++) {
+      if (_contacts[i].phoneNumber == phoneNumber ||
+          _contacts[i].name.toLowerCase() == name) {
+        return i;
+      }
+    }
+
+    return -1;
   }
 
-  void printSpecificContact() {}
+  void printSpecificContact() {
+    int position = searchContact();
+    if(position<0){print("Contact Not Found !");}
+    else{
+      print(
+          "Name : ${_contacts[position].name}\nPhone : 0${_contacts[position].phoneNumber}\nBirthDay : ${_contacts[position].birthday}");
+    }
+  }
 
-  void ModifyContact(){}
+  void modifyContact() {}
 }
 
 void menu() {
   print('----- Welcome to Contact Folder -----\n'
       '------------------- Menu ----------------\n'
-      '1 - Add Contact\n2- Search Contact\n3- Delete Contact\n4- Print Specific Contact\n5- Print All Contacts\n'
+      '1 - Add Contact\n2- Search Contact\n3- Delete Contact\n4- Print Specific Contact\n5- Print All Contacts\n0- Exit Program\n'
       '---------------Choose One -----------------\n'
+      'Your answer : ');
+}
+
+void searchMenu(){
+  print('----- Search Contact -----\n'
+      '1 - Search by Name\n 2- Search by Phone\n'
+      '---------------Choose One ---------------\n'
       'Your answer : ');
 }
 
@@ -71,7 +102,7 @@ void main() {
   String name;
   String birthday;
   int menuIndex;
-
+do {
   menu();
   menuIndex = int.parse(stdin.readLineSync()!);
 
@@ -81,13 +112,34 @@ void main() {
       phoneNumber = int.parse(stdin.readLineSync()!);
       print('name : ');
       name = stdin.readLineSync().toString();
-      print('birthday : ');
+      print('birthday (YYYY/MM/DD) : ');
       birthday = stdin.readLineSync().toString();
 
       Operation(phoneNumber: phoneNumber, name: name, birthday: birthday)
           .addContact();
+
+      break;
+    case 2 :
+      int place;
+      p:
+      searchMenu();
+      place = int.parse(stdin.readLineSync()!);
+      switch (place) {
+        case 1 :
+          print("Enter name :");
+          name = stdin.readLineSync().toString().toLowerCase();
+          if (Operation(name: name).searchContact() == -1) {
+            print("Contact Not Found !\n");
+          }
+          else {
+            Operation(name: name).printSpecificContact();
+          }
+          break;
+      }
+
       break;
   }
-
+}while(menuIndex>0);
   Operation().printAllContact();
+
 }
